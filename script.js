24821c16c36c557a350e170bab5c9060659b3509
Leny Mory <leny.mory@divtec.ch>
2024-08-27 16:19:06 +0200

    version au 27.08

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+// Recuperate le premier <h2> du document
+let compteur = 0;
+
+
+const compteurEl = document.getElementById("compteur-el");
+const sauvegardeEl = document.getElementById("sauvegarde-el");
+
+document.getElementById("capturer-btn").addEventListener("click", capturer);
+document.getElementById("sauvegarder-btn").addEventListener("click", sauvegarder);
+
+function capturer() {
+    compteur++;
+    compteurEl.textContent = compteur;
+    // Change la couleur du texte en fonction du nombre de Pokémons capturés
+    if (compteur < 5) {
+        compteurEl.style.color = 'green';
+    } else if (compteur < 10) {
+        compteurEl.style.color = 'yellow';
+    } else {
+        compteurEl.style.color = 'red';
+    }
+}
+
+function sauvegarder() {
+    if (compteur !== 0) {
+        sauvegardeEl.textContent += compteur + " Pokémons" + " - ";
+        // remettre le compteur à zéro
+        localStorage.setItem("captures", sauvegardeEl.textContent);
+        compteur = 0;
+        compteurEl.textContent = compteur;
+    }
+}
+
+// Quand l'onglet du navigateur à fini de se charger
+Window.addEventListener("load", () => {
+    sauvegardeEl.textContent = localStorage.getItem('captures');
+});
